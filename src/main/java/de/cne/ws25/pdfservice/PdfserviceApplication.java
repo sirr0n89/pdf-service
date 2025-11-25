8ffff64140cbdeb8d48365bee52f7a4fc416192f
christianseelert <chse1001@stud.hs-kl.de>
2025-11-25 12:26:29 +0100

    Initial commit: pdf-service

---
+++ src/main/java/de/cne/ws25/pdfservice/PdfserviceApplication.java
@@ -0,0 +1,13 @@
+package de.cne.ws25.pdfservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/cne/ws25/pdfservice/PdfserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cne.ws25.pdfservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
