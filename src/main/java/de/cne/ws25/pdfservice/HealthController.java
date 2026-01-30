2ba174d6d029548913521a2dff35ffc42a0c480c
christianseelert <chse1001@stud.hs-kl.de>
2026-01-30 11:21:23 +0100

    Code Comments

--- src/main/java/de/cne/ws25/pdfservice/HealthController.java
+++ src/main/java/de/cne/ws25/pdfservice/HealthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
+
     @GetMapping("/health")
     public String health() {
         return "PDF-Service läuft!";
